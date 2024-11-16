c7625e04e932e66c6141d20d47ac52250db4b133
NS258 <154657596+nsgowebjavaprog@users.noreply.github.com>
2024-11-16 18:16:00 +0530

    Add files via upload
    added

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const userModel = require('./models/user');
 
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/testapp1', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const userModel = require('./usermodule');
+
+app.get('/', (req, res) =>{
+    res.send("Hey");
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Error connecting to MongoDB:', error.message));
 
-// Set up view engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, 'views'));
+//Create
+app.get('/create', async (req, res) =>{
+    let createduser =  await userModel.create({
+        name:"Nagaraj",
+        email:"nsloni258@gmail.com",
+        username:"nsloni"
+    })
+    res.send(createduser);
+})
 
-// Middleware setup
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+//Updates
+app.get('/update', async (req, res) =>{
+ 
+    // userModel.findOneUpdate(findone, update, {new:true})
+    let updateduser = await userModel.findOneAndUpdate({username:"Nagaraj"}, {name: "Nagaraj Loni"}, {new: true});
+    res.send(updateduser);
+})
 
-// Routes
+//Read
 
-// file-1: Root route
-app.get('/', (req, res) => {
-    res.render("index");
-});
+app.get("/read", async (req, res) =>{
+    let users = await userModel.find({username:"Nagaraj"});  // or findOne
+    res.send(users);
+})
 
-app.get('/read', async (req, res) => {
-    let users = await userModel.find();
-    res.render("read", {users});
-});
 
-app.get('/edit/:userid', async (req, res) => {
-    let users = await userModel.findOne({_id: req.params.userid});
-    res.render("edit", {users});
-});
+//Delete
+app.get("/delete", async (req, res) =>{
+    let users = await userModel.findOneAndDelete({username:"Nagaraj"});
+    res.send(users);
+})
 
-app.get('/update/:userid', async (req, res) => {
-    let {image, name, email} = req.body;
-    let users = await userModel.findOneAndUpdate({_id: req.params.userid}, {image, name, email},{new:true});
-    res.redirect("/read");
-});
 
-app.post('/delete/:id', async (req, res) => {
-    let user = await userModel.findOneAndDelete({_id: req.params.id});
-    res.redirect("/read");
-});
 
-app.post('/create', async (req, res) => {
-    let { name, email, image } = req.body;
-    let createUser = await userModel.create({
-        name,
-        email,
-        image
-    });
-    res.redirect("/read"); 
-});
-
-// Start the server
-const port = 3002;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+app.listen(3000);
